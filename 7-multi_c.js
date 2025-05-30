0c48ecb33c6260512038b1087cad588b2fd9e7a0
Uthman <osquare98@gmail.com>
2025-05-30 12:02:15 +0100

    sdw

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,9 +1,22 @@
-const args = process.argv.slice(2);
+/* const args = process.argv.slice(2);
 const x = Number(args[0]);
 
 if (Number.isInteger(x) && x > 0) {
   for (let i = 0; i < x; i++) {
     console.log("C is fun");
+  }
+} else {
+  console.log("Missing number of occurrences");
+} */
+
+const args = process.argv.slice(2);
+const x = Number(args[0]);
+
+if (Number.isInteger(x) && x > 0) {
+  let i = 0;
+  while (i < x) {
+    console.log("C is fun");
+    i++;
   }
 } else {
   console.log("Missing number of occurrences");
