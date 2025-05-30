e5811c2912b57d52e7d08ff1ca5aef5a2dfaeb3e
Uthman <osquare98@gmail.com>
2025-05-30 11:25:48 +0100

    koll

--- 4-concat.js
+++ 4-concat.js
@@ -1,4 +1,8 @@
 const myVar0 = "Python";
-const myVar1 = "fun";
+/* const myVar1 = "fun";
 
-console.log(`${myVar0} is ${myVar1}`);
+console.log(`${myVar0} is ${myVar1}`); */
+
+const args = process.argv.slice(2);
+
+console.log(`${args[0]} is ${args[1]}`);
