ed26bf01767b62592463c722c7c5020102df6a4b
Uthman <osquare98@gmail.com>
2025-05-30 11:36:33 +0100

    sees

---
+++ 5-to_interger.js
@@ -0,0 +1,12 @@
+const myNumber = ["boy", 5, "5"]
+
+function numberConverter(param) {
+  if (typeof(param) === Number)
+    console.log(param)
+  else
+    console.log("Not a number")
+};
+
+console.log(myNumber[0])
+console.log(myNumber[1])
+console.log(myNumber[2])
