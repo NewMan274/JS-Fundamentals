da241bcffe1cd87f9de4a898ecb28b242421d1ce
Uthman <osquare98@gmail.com>
2025-05-30 11:43:25 +0100

    mk

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,4 +1,4 @@
-const myNumber = ["boy", 5, "5"]
+/* const myNumber = ["boy", 5, "5"]
 
 function numberConverter(param) {
   if (typeof(param) === Number)
@@ -9,4 +9,13 @@ function numberConverter(param) {
 
 console.log(numberConverter(myNumber[0]))
 console.log(numberConverter(myNumber[1]))
-console.log(numberConverter(myNumber[2]))
+console.log(numberConverter(myNumber[2])) */
+
+const args = process.argv.slice(2);
+const num = Number(args[0]);
+
+if (!isNaN(num)) {
+  console.log(`My number: ${num}`);
+} else {
+  console.log("Not a number");
+}
