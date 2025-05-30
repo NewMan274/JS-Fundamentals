b9c2bcc183274924eb64287134f8593da51d98a5
Uthman <osquare98@gmail.com>
2025-05-30 10:46:26 +0100

    ll

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,11 +1,11 @@
-const myVar = [
-  "JavaScript is amazing", 
-  "Python is cool", 
-  "JavaScript is amazing"
-] 
+const myVar0  = "JavaScript is amazing"; 
+const myVar1 =  "Python is cool";
+const myVar2 =  "JavaScript is amazing";
 
 function printing() {
-  console.log(myVar)
+  console.log(myVar0)
+  console.log(myVar1)
+  console.log(myVar2)
 };
 
 printing();
